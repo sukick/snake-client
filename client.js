b56b6aa7d4cdf25adf7121fc2b41b7c58cd89f3a
vagrant <suki.ckchan@gmail.com>
2021-03-11 06:22:49 +0000

    client.js

--- client.js
+++ client.js
@@ -1,20 +1,23 @@
 const net = require('net');
-
+const { IP, PORT } = require('./constants');
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
+    host: IP,
+    port: PORT,
   });
   // , () => { console.log("you ded cuz you idled");
-
   conn.setEncoding('utf8'); 
   conn.on('connect',() => {
     console.log("Sucessfully connected to game server");
+  });
+  conn.on('data', data => {
+    console.log("Server says: Hellooooooo ", data);
+  });
+  conn.on('connect', () => {
     conn.write("Name: CCK");
   });
     // conn.write("Move: up");
   return conn;
 };
 
-module.exports = { connect 
-}; 
+module.exports = { connect }; 
