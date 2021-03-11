d83c10ff6829ac577456e976606d087c02687529
vagrant <suki.ckchan@gmail.com>
2021-03-11 01:05:45 +0000

    play.js

---
+++ play.js
@@ -0,0 +1,36 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '<IP ADDRESS HERE>',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '<IP ADDRESS HERE>',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
