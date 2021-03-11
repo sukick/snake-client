6825d34fd080ae52cbc1a9cad3b29e24e68de973
vagrant <suki.ckchan@gmail.com>
2021-03-11 04:24:28 +0000

    play.js

--- play.js
+++ play.js
@@ -1,23 +1,4 @@
-const net = require('net');
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-  }, () => { console.log("you ded cuz you idled");
-
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  // conn.addListener("idling", function(){
-  //   console.log("you ded cuz you idled");
-  // // })
-  // console.log(conn);
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
 
